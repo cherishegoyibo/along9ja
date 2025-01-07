33ee56357b9fd2b1a3d1b5a3daa980c27def0a34
Femi Mehalayese <megafemclick@gmail.com>
2025-01-07 14:57:29 +0100

    test mode

--- Routes/direction.js
+++ Routes/direction.js
@@ -1,6 +1,6 @@
 // include file
 import  userRequest from '../userRequest.js'
-import express from 'express';
+import express, { json } from 'express';
 
 const app = express();
 
@@ -12,12 +12,27 @@ app.route('/direction')
         res.send(`GET request to the direction page`);
     })
     .post(async (req, res) => {
-        const requestData = JSON.stringify(req.body);
+        // const requestData = JSON.stringify(req.body);
+        const requestData = {
+            "userLocation": {
+              "latitude": 8.981089468375945,
+              "longitude": 7.562370306450092
+            },
+            "destination": {
+              "latitude": 9.064536267462163,
+              "longitude": 7.458264025185316
+            }
+        };
+
         const userLocation = requestData.userLocation;
         const destination = requestData.destination;
+        console.log("form detail:", userLocation, destination);
+        console.log("userlocation", userLocation.latitude, userLocation.longitude);
+        console.log("Destination", destination.latitude, destination.longitude);
         try {
             const sender = await userRequest(userLocation, destination);
-            res.send(JSON.stringify.sender);
+            console.log(sender);
+            res.send(JSON.stringify(sender));
         }
         catch(err) {
             res.status(500).send('Error, try again.')
@@ -32,6 +47,6 @@ app.route('/direction/new')
         const requestData = JSON.stringify(req.body);
     });
 
-app.listen(3000, () => {
+app.listen(3003, () => {
     console.log('Server is running on port 3000');
 });
