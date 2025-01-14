ca48d04df75f8a69927066bc5f781a471b45f057
Femi Mehalayese <megafemclick@gmail.com>
2025-01-14 10:56:07 +0100

    backend completed

--- Routes/direction.js
+++ Routes/direction.js
@@ -1,31 +1,29 @@
 // include file
-import  userRequest from '../funcs/userRequest.js'
+import userRequest from '../funcs/userRequest.js'
 import { getDirections } from '../funcs/mapDirectionRequest.js';
-import express from 'express';
+import { Router } from 'express';
 
-const app = express();
+const router = Router();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+router.use(express.json());
+router.use(express.urlencoded({ extended: true}));
 
-app.route('/direction')
+router.route('/')
     .get((req, res) => {
         res.send(`GET request to the direction page`);
     })
     .post(async (req, res) => {
-        const requestData = JSON.stringify(req.body);
-        const userLocation = requestData.userLocation;
-        const destination = requestData.destination;
+        const { userLocation, destination } = req.body;
         try {
             const directionsDetail = await userRequest(userLocation, destination);
             res.send(directionsDetail);
         }
         catch(err) {
-            res.status(500).send('Error, try again.')
+            res.status(500).send('Error, try again.');
         }
     });
 
-// app.route('/direction/new')
+// router.route('/new')
 //     .get((req, res) => {
 //         res.send(`GET request to the new direction page`);
 //     })
@@ -33,12 +31,10 @@ app.route('/direction')
 //         const requestData = JSON.stringify(req.body);
 //     });
 
-app.route('/updatedirection')
+router.route('/update')
     .post((req, res) => {
         const mapout = getDirections(req.body.from, req.body.to);
         res.send(mapout);
     });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+    export default router;
