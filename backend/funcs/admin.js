f9d97a9084d8bff8556d11df032066ce05a2e48d
Femi Mehalayese <megafemclick@gmail.com>
2025-01-26 11:31:01 +0100

    admin class created

--- backend/funcs/admin.js
+++ backend/funcs/admin.js
@@ -1,2 +1,33 @@
+import { User, Route} from '../db_model/database.js';
+import { Admin as AdminDB } from '../db_model/database.js';
+
 class Admin {
+    constructor() {
+        this.User = User;
+        this.Route = Route;
+        this.AdminDB = AdminDB;
+    }
+
+    getAllRoutes() {
+        const Routes =  this.Route.find();
+        return Routes;
+    }
+
+    getAllUsers() {
+        const Users =  this.User.find();
+        return Users;
+    }
+
+    getAllAdmins() {
+        const Admins =  this.AdminDB.find();
+        return Admins;
+    }
+
+    UpdateRoutes(id, data) {
+        const UpdateRouteRoute =  this.Route.findByIdAndUpdate(id, data);
+        if (UpdateRouteRoute) {
+            return 'Route Updated';
+        }
+
+    }
 }
